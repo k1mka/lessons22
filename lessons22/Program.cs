42403160cb363c6bc1d8cbeb7dc913172523b572
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-05-10 18:25:03 +0300

    Add project files.

---
+++ lessons22/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace lessons22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+
+            string result = str ?? string.Empty;
+
+            Console.WriteLine("колличество символов в строке: " + result);
+        }
+    }
+}
